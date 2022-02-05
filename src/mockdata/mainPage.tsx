9a769625858d329697a6759639858202e4807767
tyoohoon <57979899+tyoohoon@users.noreply.github.com>
2022-02-06 00:29:18 +0700

    create mock data
    keywordList, diagnosisList

---
+++ src/mockdata/mainPage.tsx
@@ -0,0 +1,36 @@
+export const keywordList = ['ไอ', 'ไอแบบมีเสมหะ', 'ไอเป็นเลือด', 'ไอแห้ง', 'ไข้', 'หอบ', 'คัดจมูก', 'ตาแดง']
+export const diagnosisList = [
+    {
+        diagnosis: 'กกกกกกกกกกก',
+        probability: '80.00%',
+        keyword: {
+            a: 'inactive',
+            b: 'inactive',
+            c: 'inactive',
+        }
+    }, {
+        diagnosis: 'ขขขขขขขขขขข',
+        probability: '75.00%',
+        keyword: {
+            a: 'inactive',
+            b: 'inactive',
+            c: 'inactive',
+        }
+    }, {
+        diagnosis: 'คคคคคคคคคคคคคคคค',
+        probability: '75.00%',
+        keyword: {
+            a: 'inactive',
+            b: 'inactive',
+            c: 'inactive',
+        }
+    }, {
+        diagnosis: 'งงงงงงงงงง',
+        probability: '60.50%',
+        keyword: {
+            a: 'inactive',
+            b: 'inactive',
+            c: 'inactive',
+        }
+    }
+]
